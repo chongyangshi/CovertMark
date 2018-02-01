378d3a069f29ca13da2cd593e2cf6f6d9528c756
icydoge <icydoge@gmail.com>
2018-02-01 15:48:45 +0000

    MeanShift packet length clustering for meek.

--- CovertMark/analytics/traffic.py
+++ CovertMark/analytics/traffic.py
@@ -1,9 +1,51 @@
 from analytics import constants, entropy
 
-class TrafficAnalyser:
+import numpy as np
+from scipy import stats
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from operator import itemgetter
+
+"""
+Record and analyse windowed and non-windowed packet flow statistics.
+"""
+
+def ordered_tcp_payload_length_frequency(traces, tls_only=False, bandwidth=3):
     """
-    Record and analyse windowed and non-windowed packet flow statistics.
+    Utilises meanshift to cluster input tcp frames by their payload to within
+    a certain difference (bandwidth), and return up to top five clusters.
+    This is useful if the PT sends a lot of unidirectional equal or similar
+    length payloads, for which the traces should have been filtered by source or
+    destination IP.
+    :param trace: a list of parsed packets, non-tcp packets will be ignored.
+    :param tls_only: boolean value that if True ignoring non-TLS frames,
+        including TCP frames not containing TLS headers but segmented TLS data.
+    :param bandwidth: the maximum distance within clusters, i.e. max difference
+        between payload lengths.
+    :returns: a list of sets containing values in up to five top clusters.
     """
 
-    def __init__(self, traces):
-        pass # TODO: continue from here.
+    # Collect the lengths.
+    lengths = []
+    for trace in traces:
+        if trace['tcp_info'] is None:
+            continue
+        elif tls_only and trace['tls_info'] is None:
+            continue
+        else:
+            lengths.append(len(trace['tcp_info']['payload']))
+
+    # Cluster the lengths.
+    lengths = np.array(list(zip(lengths, np.zeros(len(lengths)))), dtype=np.int)
+    meanshift = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    meanshift.fit(lengths)
+    labels = meanshift.labels_
+    labels_unique = np.unique(labels)
+    n_clusters_ = len(labels_unique)
+
+    # Return the top clusters in order.
+    clusters = []
+    for i in range(n_clusters_):
+        members = (labels == i)
+        clusters.append(set(lengths[members, 0]))
+
+    return clusters
---
+++ CovertMark/tests/meek_test.py
@@ -0,0 +1,67 @@
+from data import parser, mongo, constants
+from analytics import traffic
+
+import os, sys
+from base64 import b64decode
+
+# Temporary test script, not an actual test.
+
+m = mongo.MongoDBManager()
+parent_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+long_path = os.path.join(parent_path, 'examples', 'local')
+
+a = parser.PCAPParser(os.path.join(long_path, 'meeklong.pcap'))
+a.set_ip_filter([('192.168.0.42', constants.IP_SRC), ('13.32.68.163', constants.IP_DST)])
+name = a.load_and_insert_new("meek")
+total = m.count_traces(name, {})
+print("In total {} client->server traces by meek.".format(total))
+
+traces = m.find_traces(name, {"tls_info": {"$ne": None}})
+for trace in traces: # To be done in data.retrieve when properly implemented.
+    trace['tcp_info']['payload'] = b64decode(trace['tcp_info']['payload'])
+most_frequent = traffic.ordered_tcp_payload_length_frequency(traces, True)
+
+top_cluster = most_frequent[0]
+top_two_clusters = top_cluster.union(most_frequent[1])
+
+a = parser.PCAPParser(os.path.join(long_path, 'unobfuscatedlong.pcap'))
+a.set_ip_filter([('172.28.195.198', constants.IP_SRC)])
+name = a.load_and_insert_new("unobfuscated")
+total = m.count_traces(name, {"tcp_info": {"$ne": None}})
+all_dst_ips = m.distinct_traces(name, 'dst')
+print("In total {} client->server unobfuscated traces.".format(total))
+
+traces = m.find_traces(name, {"tls_info": {"$ne": None}})
+top = 0
+top2 = 0
+ips_blocked = set([])
+ips_blocked_2 = set([])
+for trace in traces: # To be done in data.retrieve when properly implemented.
+    payload_len = len(b64decode(trace['tcp_info']['payload']))
+    if payload_len in top_cluster:
+        top += 1
+        ips_blocked.add(trace['dst'])
+    elif payload_len in top_two_clusters:
+        top2 += 1
+        ips_blocked_2.add(trace['dst'])
+
+print("---")
+print("Top cluster False Positive Rate: {} out of {} client->server traces ({:0.2f}%)".format(top, total, float(top)/total*100))
+print("Top cluster blocked IPs: {} out of {} ({:0.2f}%).".format(len(ips_blocked), all_dst_ips, float(len(ips_blocked))/all_dst_ips*100))
+print("---")
+print("Top two clusters False Positive Rate: {} out of {} client->server traces ({:0.2f}%)".format(top2, total, float(top2)/total*100))
+print("Top two clusters blocked IPs: {} out of {} ({:0.2f}%).".format(len(ips_blocked_2), all_dst_ips, float(len(ips_blocked_2))/all_dst_ips*100))
+
+wireshark_output = "Top cluster blocked Wireshark: tcp.payload && "
+for i, ip in enumerate(list(ips_blocked)):
+    wireshark_output += "ip.dst_host == \"" + ip + "\" "
+    if i < len(ips_blocked) - 1:
+        wireshark_output += "|| "
+print(wireshark_output)
+
+wireshark_output = "Top two clusters blocked Wireshark: tcp.payload && "
+for i, ip in enumerate(list(ips_blocked_2)):
+    wireshark_output += "ip.dst_host == \"" + ip + "\" "
+    if i < len(ips_blocked_2) - 1:
+        wireshark_output += "|| "
+print(wireshark_output)
