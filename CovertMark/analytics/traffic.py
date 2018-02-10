7d8243cdb5cee5a2bd31cfdb3345bf93d63ef3c5
icydoge <icydoge@gmail.com>
2018-02-10 09:46:02 +0000

    Add UDP clustering.

--- CovertMark/analytics/traffic.py
+++ CovertMark/analytics/traffic.py
@@ -12,16 +12,17 @@ Record and analyse windowed and non-windowed packet flow statistics.
 def ordered_tcp_payload_length_frequency(traces, tls_only=False, bandwidth=3):
     """
     Utilises meanshift to cluster input tcp frames by their payload to within
-    a certain difference (bandwidth), and return up to top five clusters.
+    a certain difference (bandwidth), and return descending ordered clusters.
     This is useful if the PT sends a lot of unidirectional equal or similar
     length payloads, for which the traces should have been filtered by source or
     destination IP.
-    :param trace: a list of parsed packets, non-tcp packets will be ignored.
+    :param traces: a list of parsed packets, non-tcp packets will be ignored.
     :param tls_only: boolean value that if True ignoring non-TLS frames,
         including TCP frames not containing TLS headers but segmented TLS data.
     :param bandwidth: the maximum distance within clusters, i.e. max difference
         between payload lengths.
-    :returns: a list of sets containing values in up to five top clusters.
+    :returns: a list of sets containing clustered values ordered from most
+        frequent to least.
     """
 
     # Collect the lengths.
@@ -33,6 +34,45 @@ def ordered_tcp_payload_length_frequency(traces, tls_only=False, bandwidth=3):
             continue
         else:
             lengths.append(len(trace['tcp_info']['payload']))
+
+    # Cluster the lengths.
+    lengths = np.array(list(zip(lengths, np.zeros(len(lengths)))), dtype=np.int)
+    meanshift = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    meanshift.fit(lengths)
+    labels = meanshift.labels_
+    labels_unique = np.unique(labels)
+    n_clusters_ = len(labels_unique)
+
+    # Return the top clusters in order.
+    clusters = []
+    for i in range(n_clusters_):
+        members = (labels == i)
+        clusters.append(set(lengths[members, 0]))
+
+    return clusters
+
+
+def ordered_udp_payload_length_frequency(traces, bandwidth=3):
+    """
+    Utilises meanshift to cluster input udp frames by their packet length to within
+    a certain difference (bandwidth), and return descending ordered clusters.
+    This is useful if the PT sends a lot of unidirectional equal or similar UDP
+    length payloads, for which the traces should have been filtered by source or
+    destination IP.
+    :param traces: a list of parsed packets, non-udp packets will be ignored.
+    :param bandwidth: the maximum distance within clusters, i.e. max difference
+        between payload lengths.
+    :returns: a list of sets containing clustered values ordered from most
+        frequent to least.
+    """
+
+    # Collect the lengths.
+    lengths = []
+    for trace in traces:
+        if trace['proto'] != "UDP":
+            continue
+        else:
+            lengths.append(trace['len'])
 
     # Cluster the lengths.
     lengths = np.array(list(zip(lengths, np.zeros(len(lengths)))), dtype=np.int)
