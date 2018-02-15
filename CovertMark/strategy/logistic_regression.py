cf91dcc7e2f2fbbb95b605f3fad4e163d1d1c288
icydoge <icydoge@gmail.com>
2018-02-15 13:46:27 +0000

    Preliminary LR work completed.

--- CovertMark/strategy/logistic_regression.py
+++ CovertMark/strategy/logistic_regression.py
@@ -25,6 +25,8 @@ class LRStrategy(DetectionStrategy):
 
     def __init__(self, pt_pcap, negative_pcap=None):
         super().__init__(pt_pcap, negative_pcap, self.DEBUG)
+        self.LR = linear_model.LogisticRegression(penalty='l1', dual=False,
+         solver='saga', n_jobs=-1)
 
 
     def set_strategic_filter(self):
@@ -55,42 +57,60 @@ class LRStrategy(DetectionStrategy):
 
     def positive_run(self, **kwargs):
         """
-        Three different criteria of combing results from KS byte-uniformity, Entropy
-        Distribution, and Anderson_Darling tests together, all using p=0.1 as
-        the hypothesis rejection threshold, with the latter two using a byte
-        block size of 8. Reporting a selected criterion.
-        :param block_size: the size of blocks of payload bytes tested in KS and
-            AD. Default set in self.BLOCK_SIZE.
-        :param p_threshold: the p-value threshold at which uniform random
-            hypothesis can be rejected, defaulted at 0.1.
+        Perform logistic regression on the training/testing dataset, and validate
+        overfitting on validation dataset.
         """
 
+        self.debug_print("- Logistic Regression training with L1 penalisation and SAGA solver...")
+        self.LR.fit(self._pt_test_traces, self._pt_test_labels)
 
-    def negative_run(self, **kwargs):
+        self.debug_print("- Logistic Regression validation...")
+        prediction = self.LR.predict(self._pt_validation_traces)
+
+        total_positives = 0
+        true_positives = 0
+        false_positives = 0
+        total_negatives = 0
+        true_negatives = 0
+        false_negatives = 0
+        for i in range(0, len(prediction)):
+            if prediction[i] == 1: # Positive identification
+                total_positives += 1
+                if self._pt_validation_labels[i] == 1:
+                    true_positives += 1
+                else:
+                    false_positives += 1
+            else:
+                total_negatives += 1
+                if self._pt_validation_labels[i] == 0:
+                    true_negatives += 1
+                else:
+                    false_negatives += 1
+
+        self._strategic_states["total"] = total_positives + total_negatives
+        self._strategic_states["TPR"] = float(true_positives) / self._strategic_states["total"]
+        self._strategic_states["FPR"] = float(false_positives) / self._strategic_states["total"]
+        self._strategic_states["TNR"] = float(true_negatives) / self._strategic_states["total"]
+        self._strategic_states["FNR"] = float(false_negatives) / self._strategic_states["total"]
+
+        return self._strategic_states["TPR"]
+
+
+    def negative_run(self):
         """
-        Test the same thing on negative traces. Reporting blocked IPs.
-        :param block_size: the size of blocks of payload bytes tested in KS and
-            AD. Default set in self.BLOCK_SIZE.
-        :param p_threshold: the p-value threshold at which uniform random
-            hypothesis can be rejected, defaulted at 0.1.
+        Not used at this time, as FPR combined into self.positive_run.
         """
+
+        return None
 
 
     def report_blocked_ips(self):
         """
-        Return a Wireshark-compatible filter expression to allow viewing blocked
-        traces in Wireshark. Useful for studying false positives.
-        :returns: a Wireshark-compatible filter expression string.
+        It is not possible to pin point blocked IPs in the false positive cases,
+        as the traces have been windowed.
         """
 
-        wireshark_output = "tcp && "
-        for i, ip in enumerate(list(self._negative_blocked_ips)):
-            wireshark_output += "ip.dst_host == \"" + ip + "\" "
-            if i < len(self._negative_blocked_ips) - 1:
-                wireshark_output += "|| "
-        wireshark_output += ")"
-
-        return wireshark_output
+        return "Unavailable for logistic regression."
 
 
     def run(self, pt_ip_filters=[], negative_ip_filters=[], pt_split=True, pt_split_ratio=0.5):
@@ -135,7 +155,7 @@ class LRStrategy(DetectionStrategy):
                 continue
             negative_features.append([i[1] for i in sorted(feature_dict.items(), key=itemgetter(0))])
 
-        self.debug_print("Prepared {} PT windows, {} negative windows.".format(\
+        self.debug_print("Prepared {} positive windows, {} negative windows.".format(\
          len(positive_features), len(negative_features)))
         all_features = positive_features + negative_features
         all_features = np.asarray(all_features, dtype=np.float64)
@@ -157,10 +177,18 @@ class LRStrategy(DetectionStrategy):
         self._pt_test_labels = self._strategic_states['training_labels']
         self._pt_validation_labels = self._strategic_states['validation_labels']
 
-        # No longer required, no idea if it actually frees up memory in interpreter.
+        # Stored states no longer required.
         self._strategic_states = {}
 
+        # Run training and validation.
+        self._run_on_positive()
 
+        self.debug_print("Results of validation: ")
+        self.debug_print("Total: {}".format(self._strategic_states["total"]))
+        self.debug_print("TPR: {:0.2f}%, TNR: {:0.2f}%".format(\
+         self._strategic_states['TPR']*100, self._strategic_states['TNR']*100))
+        self.debug_print("FPR: {:0.2f}%, FNR: {:0.2f}%".format(\
+         self._strategic_states['FPR']*100, self._strategic_states['FNR']*100))
 
         return (self._true_positive_rate, self._false_positive_rate)
 
@@ -169,11 +197,10 @@ if __name__ == "__main__":
     parent_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
     # Longer ACS Test.
-    lr_path = os.path.join(parent_path, 'examples', 'meek.pcap')
-    unobfuscated_path = os.path.join(parent_path, 'examples', 'unobfuscated.pcap')
+    lr_path = os.path.join(parent_path, 'examples', 'local', 'meeklong.pcap')
+    unobfuscated_path = os.path.join(parent_path, 'examples', 'local', 'unobfuscated_acstest.pcap')
     detector = LRStrategy(lr_path, unobfuscated_path)
-    detector.run(pt_ip_filters=[('172.28.192.0/24', data.constants.IP_EITHER)],
-        negative_ip_filters=[('172.28.192.0/24', data.constants.IP_EITHER)])
+    detector.run(pt_ip_filters=[('192.168.0.42', data.constants.IP_EITHER)],
+        negative_ip_filters=[('128.232.17.20', data.constants.IP_EITHER)])
 
     detector.clean_up_mongo()
-    print(detector.report_blocked_ips())
