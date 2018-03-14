2c4b307d730c9de208faa7970dc765c3c9cbbedb
icydoge <icydoge@gmail.com>
2018-03-14 17:29:01 +0000

    Validate filter and filter map lengths.

--- CovertMark/covertmark.py
+++ CovertMark/covertmark.py
@@ -28,6 +28,9 @@ for strategy in strategies.values():
         if c not in strategy or not isinstance(strategy[c], t):
             good_json = False
 
+    pt_filters_len = len(strategy["pt_filters"])
+    neg_filters_len = len(strategy["negative_filters"])
+
     # These are the parameters always applied to the strategy.run(..) call.
     for i in strategy["fixed_params"]:
         if len(i) != 2 or not i[0].isidentifier():
@@ -58,11 +61,17 @@ for strategy in strategies.values():
             else:
                 good_json = False
 
+        if len(r["pt_filters_map"]) != pt_filters_len:
+            good_json = False
+
         for i, w in enumerate(r["negative_filters_map"]):
             if w in strategy_constants.JSON_FILTERS:
                 r["negative_filters_map"][i] = strategy_constants.JSON_FILTERS[w]
             else:
                 good_json = False
+
+        if len(r["negative_filters_map"]) != neg_filters_len:
+            good_json = False
 
         # These are the parameters applied to the strategy.run(..) call with
         # only expected int/str/bool type defined, requiring the user to
