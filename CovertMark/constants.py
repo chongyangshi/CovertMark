ffcc375471b40c471156d92d85bb94ffae29fb5d
icydoge <icydoge@gmail.com>
2018-03-15 00:58:33 +0000

    Add a terminal program title.

--- CovertMark/constants.py
+++ CovertMark/constants.py
@@ -1,4 +1,23 @@
-class c:
+# Application info.
+CM_NAME = "CovertMark"
+CM_VER = "0.1"
+CM_RELEASE = "alpha"
+CM_AUTHOR = "C Shi"
+CM_LINK = "https://github.com/icydoge"
+CM_LICENSE = "Please see LICENSE.md for terms of usage of this program."
+CM_TITLE = """\
+ _____                     _  ___  ___           _
+/  __ \                   | | |  \/  |          | |
+| /  \/ _____   _____ _ __| |_| .  . | __ _ _ __| | __
+| |    / _ \ \ / / _ | '__| __| |\/| |/ _` | '__| |/ /
+| \__/| (_) \ V |  __| |  | |_| |  | | (_| | |  |   <
+ \____/\___/ \_/ \___|_|   \__\_|  |_/\__,_|_|  |_|\_\\
+"""
+
+DIVIDER = "-" * 40
+
+# UI colours.
+class colours:
     PURPLE = '\033[95m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
--- CovertMark/covertmark.py
+++ CovertMark/covertmark.py
@@ -2,16 +2,23 @@ import os, sys
 from importlib import import_module
 
 import data, analytics, strategy
-import constants
+import constants as c
 import utils
 
-good_json, reason = utils.read_strategy_map()
+# Reader for listing collections.
+reader = data.retrieve.Retriever()
 
+# Check if the strategy map is still good.
+good_json, reason = utils.read_strategy_map()
 if not good_json:
     print("strategy/strategy_map.json is invalid after customisation, due to: ")
     print(reason)
     print("exiting...")
     sys.exit(1)
 
-# Now perform the rest of the setup.
-reader = data.retrieve.Retriever()
+# Main titles!
+print(c.CM_TITLE)
+print(" " * 20 + c.CM_NAME + " " + c.CM_VER + c.CM_RELEASE + "\n")
+print("(c) " + c.CM_AUTHOR + " (" + c.CM_LINK + ") and contributors")
+print(c.CM_LICENSE)
+print(c.DIVIDER)
